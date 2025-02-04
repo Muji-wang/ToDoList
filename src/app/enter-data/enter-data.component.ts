6fc54c93f787effed8bc6038439bb8d14302fa79
muji-wang <muji.wang@deledao.com>
2025-02-04 16:37:24 +0800

    Revert "修改後重新上傳"
    This reverts commit 83a6bd8f241eb8dcc794237f87a0694e948465b9.

--- src/app/enter-data/enter-data.component.ts
+++ src/app/enter-data/enter-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 
@@ -7,64 +7,56 @@ import { BehaviorSubject } from 'rxjs';
   templateUrl: './enter-data.component.html',
   styleUrls: ['./enter-data.component.css']
 })
-export class EnterDataComponent implements OnInit {
-  userForm!: FormGroup;
-  submittedDataList: any[] = [];
-  editingIndex: number | null = null;
-
-  countryOptions = ['台灣', '美國', '日本', '韓國'];
-  regionOptions: { [key: string]: string[] } = {
-    '台灣': ['台北','桃園','台中','台南','高雄'],
-    '美國': ['紐約','洛杉磯','芝加哥','休士頓','費城'],
-    '日本': ['東京', '大阪', '京都','橫濱','名古屋'],
-    '韓國': ['首爾','釜山','仁川','光州','大分']
+export class EnterDataComponent {
+  userForm: FormGroup;
+  countryOptions: string[] = ['台灣', '美國', '日本', '韓國'];
+  
+  regions = {
+    '台灣': ['台北', '桃園', '台中', '台南', '高雄'],
+    '美國': ['紐約', '洛杉磯', '芝加哥', '休士頓', '費城'],
+    '日本': ['東京', '大阪', '京都', '橫濱', '名古屋'],
+    '韓國': ['首爾', '釜山', '大邱', '仁川', '光州']
   };
   selectedRegion$ = new BehaviorSubject<string[]>([]);
+  submittedData: any = null;
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
     this.userForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
-      gender: ['', Validators.required],
+      gender: ['male'],
       country: ['', Validators.required],
       region: [''],
       terms: [false, Validators.requiredTrue]
     });
 
     this.userForm.get('country')?.valueChanges.subscribe(country => {
-      this.selectedRegion$.next(this.regionOptions[country] || []);
+      this.selectedRegion$.next(this.regions[country as keyof typeof this.regions] || []);
       this.userForm.get('region')?.setValue('');
     });
   }
 
   submitForm() {
     if (this.userForm.valid) {
-      const formData = this.userForm.value;
-      if (this.editingIndex !== null) {
-        this.submittedDataList[this.editingIndex] = formData;
-        this.editingIndex = null;
-      } else {
-        this.submittedDataList.push(formData);
-      }
-      this.userForm.reset();
-      this.userForm.patchValue({ terms: false });
+      this.submittedData = this.userForm.value;
+    } else {
+      alert('請填寫完整表單！');
     }
   }
 
-  editForm(index: number) {
-    this.editingIndex = index;
-    this.userForm.patchValue(this.submittedDataList[index]);
+  editForm() {
+    if (this.submittedData) {
+      this.userForm.setValue(this.submittedData);
+      this.submittedData = null;
+    }
   }
 
   exportJson() {
-    const jsonStr = JSON.stringify(this.submittedDataList, null, 2);
-    const blob = new Blob([jsonStr], { type: 'application/json' });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'form-data.json';
-    a.click();
-    window.URL.revokeObjectURL(url);
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.userForm.value, null, 2));
+    const downloadAnchor = document.createElement('a');
+    downloadAnchor.setAttribute("href", dataStr);
+    downloadAnchor.setAttribute("download", "formData.json");
+    document.body.appendChild(downloadAnchor);
+    downloadAnchor.click();
+    document.body.removeChild(downloadAnchor);
   }
 }
